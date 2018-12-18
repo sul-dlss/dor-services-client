3dcc21223fe4557a94ff14a9856aef27f0f4f576
Justin Coyne <jcoyne@justincoyne.com>
2018-12-18 12:57:00 -0600

    Refactor to prepare for more types of requests

--- lib/dor/services/client.rb
+++ lib/dor/services/client.rb
@@ -4,6 +4,8 @@ require 'dor/services/client/version'
 require 'singleton'
 require 'faraday'
 require 'active_support/core_ext/hash/indifferent_access'
+require 'dor/services/client/files'
+require 'dor/services/client/objects'
 
 module Dor
   module Services
@@ -12,6 +14,14 @@ module Dor
 
       include Singleton
 
+      def objects
+        @objects ||= Objects.new(connection: connection)
+      end
+
+      def files
+        @files ||= Files.new(connection: connection)
+      end
+
       def self.configure(url:)
         instance.url = url
       end
@@ -19,60 +29,29 @@ module Dor
       # Creates a new object in DOR
       # @return [HashWithIndifferentAccess] the response, which includes a :pid
       def self.register(params:)
-        instance.register(params: params)
+        instance.objects.register(params: params)
       end
 
       # @param [String] object the identifier for the object
       # @param [String] filename the name of the file to retrieve
       # @return [String] the file contents from the workspace
       def self.retrieve_file(object:, filename:)
-        instance.retrieve_file(object: object, filename: filename)
+        instance.files.retrieve(object: object, filename: filename)
       end
 
       # @param [String] object the identifier for the object
       # @return [Array<String>] the list of filenames in the workspace
       def self.list_files(object:)
-        instance.list_files(object: object)
+        instance.files.list(object: object)
       end
 
       attr_writer :url
 
+      private
+
       def url
         @url || raise(Error, 'url has not yet been configured')
       end
-
-      def register(params:)
-        resp = connection.post do |req|
-          req.url 'v1/objects'
-          req.headers['Content-Type'] = 'application/json'
-          req.headers['Accept'] = 'application/json' # asking the service to return JSON (else it'll be plain text)
-          req.body = params.to_json
-        end
-        raise "#{resp.reason_phrase}: #{resp.status} (#{resp.body})" unless resp.success?
-
-        JSON.parse(resp.body).with_indifferent_access
-      end
-
-      def retrieve_file(object:, filename:)
-        resp = connection.get do |req|
-          req.url "v1/objects/#{object}/contents/#{filename}"
-        end
-        return unless resp.success?
-
-        resp.body
-      end
-
-      def list_files(object:)
-        resp = connection.get do |req|
-          req.url "v1/objects/#{object}/contents"
-        end
-        return [] unless resp.success?
-
-        json = JSON.parse(resp.body)
-        json['items'].map { |item| item['name'] }
-      end
-
-      private
 
       def connection
         @connection ||= Faraday.new(url)
