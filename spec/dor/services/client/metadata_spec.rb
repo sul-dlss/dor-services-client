2d074fada48980d476fc723c4781d64c56339920
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-03-31 08:46:33 -0700

    Remove `Metadata#public_xml` which is now dead code
    Connects to sul-dlss/infrastructure-integration-test#575
    
    There was only one consumer of this endpoint---our integration test suite---and now this is no longer used. Public XML generation is moving out of DSA and into purl-fetcher.

--- spec/dor/services/client/metadata_spec.rb
+++ spec/dor/services/client/metadata_spec.rb
@@ -45,67 +45,6 @@ RSpec.describe Dor::Services::Client::Metadata do
     end
   end
 
-  describe '#public_xml' do
-    subject(:response) { client.public_xml }
-
-    before do
-      stub_request(:get, 'https://dor-services.example.com/v1/objects/druid:1234/metadata/public_xml')
-        .to_return(status: status, body: body)
-    end
-
-    context 'when the object is found' do
-      let(:status) { 200 }
-      let(:body) do
-        <<~XML
-          <publicObject id="druid:1234" published="2021-04-23T19:43:06Z" publishVersion="dor-services/9.6.2">
-            <identityMetadata/>
-            <rightsMetadata>
-              <access type="discover">
-                <machine>
-                  <none/>
-                </machine>
-              </access>
-              <access type="read">
-                <machine>
-                  <none/>
-                </machine>
-              </access>
-              <use>
-                <human type="useAndReproduction"/>
-                <human type="creativeCommons"/>
-                <machine type="creativeCommons" uri=""/>
-                <human type="openDataCommons"/>
-                <machine type="openDataCommons" uri=""/>
-              </use>
-              <copyright>
-                <human/>
-              </copyright>
-            </rightsMetadata>
-          </publicObject>
-        XML
-      end
-
-      it { is_expected.to eq body }
-    end
-
-    context 'when the object is not found' do
-      let(:status) { 404 }
-      let(:body) { '' }
-
-      it { is_expected.to be_nil }
-    end
-
-    context 'when there is a server error' do
-      let(:status) { [500, 'internal server error'] }
-      let(:body) { 'broken' }
-
-      it 'raises an error' do
-        expect { response }.to raise_error(Dor::Services::Client::UnexpectedResponse,
-                                           'internal server error: 500 (broken) for druid:1234')
-      end
-    end
-  end
-
   describe '#descriptive' do
     subject(:response) { client.descriptive }
 
